019ed5c4326347405bb064f9152e00bf3dc122f9
James Gotsell <jamesgotsell@Jamess-MacBook-Pro.local>
2018-02-12 23:24:29 +0000

    updated readMe.md and apiCallController.js

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # ricky-and-morty-express-api
+
+created index routes file - 
+
+```
+router.get('/', apiCallController.homePage);
+router.get('/data',  apiCallController.rickyNMortyData)
+router.post('/quotes' , apiCallController.addQuote)
+```
+i pass each function into the relative route
+
+# to do 
+
+* work on addQutoe function so any quote passed as route gets added to the mongoDB database
+* next function get data from  from mongoDB dataBase;
+* next function to delete quote from mongoDB
--- app/controller/apiCallController.js
+++ app/controller/apiCallController.js
@@ -1,7 +1,7 @@
 var fetch = require('node-fetch');
 const bodyParser = require('body-parser'); 
 
-const url = 'http://loremricksum.com/api/?paragraphs=1&quotes=3'
+const url = 'http://loremricksum.com/api/?paragraphs=5&quotes=3'
 exports.homePage = (req, res) => {
   res.send("hello this is my rick and morty express app")
 };
@@ -18,6 +18,7 @@ exports.rickyNMortyData = (req, res) => {
 
 exports.addQuote = (req, res , next) => {
       // i need to make sure sure that that what gets send to this endpoint is a 
+      // need to make sure that check if undefined 
       const successStatus = 200;
       const failure = 400 
       console.log(req.body)
