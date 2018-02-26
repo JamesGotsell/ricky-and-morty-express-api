2b683b6869f75053efe2e08c61a72edb2472ee2a
James Gotsell <jamesgotsell@Jamess-MacBook-Pro.local>
2018-02-26 21:55:35 +0000

    created login register / signin routes created new user scheme and npm installed brcryt for hashing  of passwords

--- app/controller/userAuthController.js
+++ app/controller/userAuthController.js
@@ -1,21 +1,67 @@
 
-var User  = require('../models/user');
-var jwt    = require('jsonwebtoken');
-var config = require('../config'); 
+// var User  = require('../models/user');
+   var mongoose = require('mongoose'),   
+      jwt    = require('jsonwebtoken'),
+      config = require('../config'),
+      bcrypt = require('bcrypt'),
+      User = mongoose.model('User')
+      salt = bcrypt.salt
 
 
-exports.login = (req, res , next) => {
-      /// need to refactor to register user and login 
-    User.create({
-      name: req.body.name, 
-      password: req.body.password,
-      admin: req.body.admin 
-    })
-
-    console.log('User saved successfully');
-    res.json({ success: true }); 
-    
+exports.register = function(req, res , next ) {
+      var newUser = new User(req.body);
+      newUser.hash_password = bcrypt.hashSync(req.body.password, salt)
+      newUser.save(function(err,user){
+            if(err){
+                  return res.status(400).send({
+                        message: err
+                  })
+            }else {
+                  user.hash_password = undefined;
+                  return res.json(user)
+            }
+      })
 }
+
+exports.sign_in = function(req, res, next) {
+      User.findOne({
+        email: req.body.email
+      }, function(err, user) {
+        if (err) throw err;
+        if (!user) {
+          res.status(401).json({ message: 'Authentication failed. User not found.' });
+        } else if (user) {
+          if (!user.comparePassword(req.body.password)) {
+            res.status(401).json({ message: 'Authentication failed. Wrong password.' });
+          } else {
+            return res.json({token: jwt.sign({ email: user.email, fullName: user.fullName, _id: user._id}, 'RESTFULAPIs')});
+          }
+        }
+      });
+};
+
+exports.loginRequired = function(req, res, next) {
+      if (req.user) {
+        next();
+      } else {
+        return res.status(401).json({ message: 'Unauthorized user!' });
+      }
+};
+
+
+
+// exports.login = (req, res , next) => {
+//       /// need to refactor to register user and login 
+//     User.create({
+//       name: req.body.name, 
+//       password: req.body.password,
+//       admin: req.body.admin 
+//     })
+
+//     console.log('User saved successfully');
+//     res.json({ success: true }); 
+    
+// }
 
 exports.users = (req, res, next) => {
       User.find({}, function(err, users) {
@@ -23,31 +69,31 @@ exports.users = (req, res, next) => {
       })  
 }
 
-exports.authenticate = (req, res, next) => {
-      console.log(req.body.name)
-      User.findOne({
-            name: req.body.name
-      }, function(err, user){
-            if(err) throw err 
-            if(!user) {
-                  res.json({ success: false, message: 'Authentication failed. User not found.' });
-            } else if (user) {
-                  if (user.password != req.body.password) {
-                        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-                      } else {
-                    const payload = {
-                      admin: user.admin 
-                    };
-                  var token = jwt.sign(payload, config.secret, {
-                          expiresIn: 1440 
-                  });
+// exports.authenticate = (req, res, next) => {
+//       console.log(req.body.name)
+//       User.findOne({
+//             name: req.body.name
+//       }, function(err, user){
+//             if(err) throw err 
+//             if(!user) {
+//                   res.json({ success: false, message: 'Authentication failed. User not found.' });
+//             } else if (user) {
+//                   if (user.password != req.body.password) {
+//                         res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+//                       } else {
+//                     const payload = {
+//                       admin: user.admin 
+//                     };
+//                   var token = jwt.sign(payload, config.secret, {
+//                           expiresIn: 1440 
+//                   });
 
-                  res.json({
-                          success: true,
-                          message: 'Enjoy your token!',
-                          token: token
-                  });
-                  }  
-            }
-      })
-}
+//                   res.json({
+//                           success: true,
+//                           message: 'Enjoy your token!',
+//                           token: token
+//                   });
+//                   }  
+//             }
+//       })
+// }
--- app/routes/index.js
+++ app/routes/index.js
@@ -5,31 +5,7 @@ const userAuthController = require('../controller/userAuthController')
 var jwt    = require('jsonwebtoken');
 var config = require('../config'); 
 
- // issue with token 
-// router.use(function (req, res, next){
-//   var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
-//   if(token) {
-//       jwt.verify(token, config.secret), function(err, decoded){
-//         if(err){
-//           return res.json({ success: false, message: 'Failed to auth token'});
-//         }else {
-//           req.decoded = decoded;
-//           next()
-//         }
-//       }
-//   }else {
-//     return res.status(403).send({ 
-//       success: false, 
-//       message: 'No token provided.' 
-//   });
-
-//   }
-// })
-
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTE5NTExNTE1LCJleHAiOjE1MTk1MTI5NTV9.PMaZD5aWfT5U37bf7Dn0Bquj4EbCQBMCrTboPdiA2nE
-
-router.post('/authenticate', userAuthController.authenticate) 
 
 router.get('/', apiCallController.homePage);
 
@@ -41,7 +17,9 @@ router.get('/get-all-fav-quotes', apiCallController.getAllFavouriteQuotes)
 
 router.delete('/quotes/delete', apiCallController.deleteFavouriteQuote)
 
-router.post('/login',  userAuthController.login)
+router.post('/login',  userAuthController.loginRequired)
+router.post('/register', userAuthController.register)
+router.post('/signin', userAuthController.sign_in)
 
 router.get('/users', userAuthController.users)
 
